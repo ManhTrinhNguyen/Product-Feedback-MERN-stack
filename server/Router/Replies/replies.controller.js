b7d13ee223ffe009aebd846e1987903e8603dd9c
Trinh Nguyen <trinhnguyen@Trinhs-MBP.cable.rcn.com>
2024-03-14 12:51:30 -0400

    Create Delete Reply Route

--- server/Router/Replies/replies.controller.js
+++ server/Router/Replies/replies.controller.js
@@ -1,12 +1,11 @@
 import { ProductFeedback } from "../../Models/productFeedbackModel/productFeedbackSchema.js"
 
-export async function httpReplies(req, res) {
+// Add Reply
+export async function httpCreateNewReplies(req, res) {
   const { feedbackId, commentId } = req.params;
   const { content } = req.body;
 
   const feedback = await ProductFeedback.findById(feedbackId);
-
-  console.log(feedback)
 
   if (!feedback) {
     return res.status(400).json({
@@ -30,4 +29,36 @@ export async function httpReplies(req, res) {
   await feedback.save();
 
   res.status(201).json(newReply)
+}
+
+// Delete Reply
+export async function httpDeleteReplies(req, res) {
+  const { feedbackId, commentId, repliesId } = req.params;
+
+  const feedback = await ProductFeedback.findById(feedbackId);
+
+  if (!feedback) {
+    return res.status(400).json({
+      error: 'No Feedback Found!'
+    })
+  };
+
+  const comment = feedback.comments.find(comment => comment.id === Number(commentId))
+
+  if (!comment) {
+    return res.status(400).json({
+      error: 'No Comment Found!'
+    })
+  };
+
+  const index = comment.replies.findIndex(reply => reply._id === repliesId);
+
+  // Remove the comment from the post's comments array
+  comment.replies.splice(index, 1);
+
+  await feedback.save()
+
+  res.status(200).json({
+    message : 'Successfully delete replies'
+  })
 }
--- server/Router/Replies/replies.router.js
+++ server/Router/Replies/replies.router.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import { httpReplies } from './replies.controller.js';
+import { httpCreateNewReplies, httpDeleteReplies } from './replies.controller.js';
 
 const repliesRouter = express.Router();
 
-repliesRouter.post('/feedback/:feedbackId/comments/:commentId/replies', httpReplies);
+repliesRouter.post('/feedback/:feedbackId/comments/:commentId/replies', httpCreateNewReplies);
+repliesRouter.delete('/feedback/:feedbackId/comments/:commentId/replies/:repliesId', httpDeleteReplies)
 
 export default repliesRouter
